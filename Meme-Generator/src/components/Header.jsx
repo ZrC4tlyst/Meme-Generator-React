ef19ad95d5820f8f0cd70c6ff13e297688918ec6
Zuhair Rehman <zuhair.rehman85@gmail.com>
2024-05-23 23:36:57 +0100

    Built the Header component

--- Meme-Generator/src/components/Header.jsx
+++ Meme-Generator/src/components/Header.jsx
@@ -1,10 +1,15 @@
+import styles from "./Header.module.css";
+import trollFace from "../assets/troll-face.png";
+
 const Header = () => {
   return (
     <header>
       <nav>
-        <img src="./assets/troll-face.png" alt="" />
-              <h1>Meme Generator</h1>
-              <h3>React Course - Project</h3>
+        <section>
+          <img src={trollFace} alt="" />
+          <h1>Meme Generator</h1>
+        </section>
+        <h3>Scrimba React Course - Project</h3>
       </nav>
     </header>
   );
