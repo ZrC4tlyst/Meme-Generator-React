3fc6941ab17570d51dda7866b60c86164fd06d4e
Zuhair Rehman <zuhair.rehman85@gmail.com>
2024-05-23 23:04:22 +0100

    first commit

---
+++ Meme-Generator/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src="./assets/troll-face.png" alt="" />
+              <h1>Meme Generator</h1>
+              <h3>React Course - Project</h3>
+      </nav>
+    </header>
+  );
+};
+export default Header;
