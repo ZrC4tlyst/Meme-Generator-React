92d6e01178f5c5faedf690ae2beaed97952a52d8
Zuhair Rehman <zuhair.rehman85@gmail.com>
2024-05-24 13:45:09 +0100

    Added memesData array and also set up event listener on memeImg button

--- Meme-Generator/src/components/Meme.jsx
+++ Meme-Generator/src/components/Meme.jsx
@@ -1,14 +1,23 @@
 import styles from "./Meme.module.css";
+import memesData from "../../memesData";
 
 const Meme = () => {
+  function getMemeData() {
+    let memesArray = memesData.data.memes;
+    let randomMemeImg = Math.floor(Math.random() * memesArray.length);
+    // eslint-disable-next-line no-unused-vars
+    const url = memesArray[randomMemeImg].url;
+    console.log(url);
+  }
+
   return (
     <>
       <div className={`${styles.form}`}>
-        <div className= {`${styles['inputs-container']}`}>
-          <input type="text" />
-          <input type="text" />
+        <div className={`${styles["inputs-container"]}`}>
+          <input type="text" placeholder="Top text" />
+          <input type="text" placeholder="Bottom text" />
         </div>
-        <button>Get new meme image 🎨</button>
+        <button onClick={getMemeData}>Get new meme image 🎨</button>
       </div>
     </>
   );
